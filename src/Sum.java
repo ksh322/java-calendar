e903692151387132b4031fca94cd51f9a5ada28d
ksh322 <44285158+ksh322@users.noreply.github.com>
2021-07-29 16:15:40 +0900

    sum

---
+++ src/Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Sum {
+	public static void main(String[] args) {
+		int a,b;
+		Scanner scanner= new Scanner (System.in);
+		String s1,s2;
+		System.out.println("두수를입력하세요");
+
+		s1= scanner.next();
+		s2= scanner.next();
+		a= Integer.parseInt(s1);
+		b= Integer.parseInt(s2);
+		int c= a+b;
+		
+		System.out.printf("두수의 합은 %d" ,a+b);
+		scanner.close();
+	}
+	
+}
